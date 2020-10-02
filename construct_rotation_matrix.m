c0695527941cae399c35209a3f6cf6811424255b
needhelp-proj <65340575+needhelp-proj@users.noreply.github.com>
2020-10-02 11:53:49 +0200

    Update construct_rotation_matrix.m

--- construct_rotation_matrix.m
+++ construct_rotation_matrix.m
@@ -1,7 +1,11 @@
 function R_mat = construct_rotation_matrix(angles)
 %Construct the rotation matrix from the euler angles
-%   Detailed explanation goes here
+% Input: angles is a vector of (omega, phi, kappa), the unit is rad
+% Output: Matrix M, which is R3(kappa)*R2(phi)*R1(omega)
 
+R_mat = [cos(angles(2))*cos(angles(3)), sin(angles(1))*sin(angles(2))*cos(angles(3))-cos(angles(1))*sin(angles(3)), sin(angles(1))*sin(angles(3))+cos(angles(1))*sin(angles(2))*cos(angles(3));
+         cos(angles(2))+sin(angles(3)), cos(angles(1))*cos(angles(3))+sin(angles(1))*sin(angles(2))*sin(angles(3)), cos(angles(1))*sin(angles(2))*sin(angles(3))-sin(angles(1))*cos(angles(3));
+         -sin(angles(2)),           sin(angles(1))*cos(angles(2)),                                  cos(angles(1))*cos(angles(2))];
 
 
 
